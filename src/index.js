2bb4e413d52eb9b4ab9ff310d859c6a943615914
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-23 12:59:09 -0500

    Import printBooksInLibrary module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import generatePalette from "./generatePalette";
 import Book from "./Book";
+import printBooksInLibrary from './printBooksInLibrary';
 
 let palettes = ['standard', 'avatar', 'percy', 'nature'];
 let lastPalette;
@@ -139,28 +140,6 @@ function addBookToLibrary(e) {
   } 
 }
 
-function printBooksInLibrary(bookList = [], container) {
-    container.innerHTML = bookList.map((book, i) => {
-      let readStatus  = book.read === true ? 'Read' : 'Not read';
-      return `
-        <div data-cardIndex="${i}" class="card ${book.colorTheme} card${i} numberID" >
-          <div class="card-wrapper ${book.colorTheme}">
-            <div class="card-details ${book.colorTheme}">
-              <h3 class="card-title">${book.title}</h3>
-              <p class="card-author">By ${book.author}</p>
-              <p class="card-pages">${book.pages} pages</p>
-            </div>
-          
-            <div class="card-tags">
-              <button data-index="${i}" class="card-readButton ${book.read}">${readStatus}</button>
-              <button data-index="${i}" class="card-colorButton">color</button>
-            </div>
-            <button data-index="${i}" class="card-removeButton">Remove</button>
-          </div>
-        </div>
-      `;
-    }).join('');
-}
 
 function handleCardClick(e) {
     let button = e.target;
