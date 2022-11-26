88df80be488ce8850d0e9fb9e03ea447ca3cc057
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-26 13:36:21 -0500

    Add printBooksInLibrary callback as input

--- src/handleCardClick.js
+++ src/handleCardClick.js
@@ -1,31 +1,32 @@
 function handleCardClick(
-    e,
-    myLibrary, 
-    cardsBox, 
-    palettes, 
-    lastPalette
-    ) {
-    let button = e.target;
-    let buttonIdx = parseInt(button.dataset.index);
-    let updatedUserInputs = myLibrary[buttonIdx];
-  
-    if (button.className.includes('card-readButton')) {
-      updatedUserInputs.read = !updatedUserInputs.read;
-      localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
-      printBooksInLibrary(myLibrary, cardsBox);
-    } 
-  
-    if (button.className.includes('card-colorButton')) {
-      updatedUserInputs.colorTheme = generatePalette(palettes, lastPalette);
-      localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
-      printBooksInLibrary(myLibrary, cardsBox);
-    } 
-  
-    if (button.className.includes('card-removeButton')) {
-      myLibrary.splice(buttonIdx, 1);
-      localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
-      printBooksInLibrary(myLibrary, cardsBox);
-    } 
+  e,
+  myLibrary, 
+  cardsBox, 
+  palettes, 
+  lastPalette,
+  printBooksInLibrary
+  ) {
+  let button = e.target;
+  let buttonIdx = parseInt(button.dataset.index);
+  let updatedUserInputs = myLibrary[buttonIdx];
+
+  if (button.className.includes('card-readButton')) {
+    updatedUserInputs.read = !updatedUserInputs.read;
+    localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
+    printBooksInLibrary(myLibrary, cardsBox);
+  } 
+
+  if (button.className.includes('card-colorButton')) {
+    updatedUserInputs.colorTheme = generatePalette(palettes, lastPalette);
+    localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
+    printBooksInLibrary(myLibrary, cardsBox);
+  } 
+
+  if (button.className.includes('card-removeButton')) {
+    myLibrary.splice(buttonIdx, 1);
+    localStorage.setItem('libraryBooks', JSON.stringify(myLibrary));
+    printBooksInLibrary(myLibrary, cardsBox);
+  } 
 };
 
 export default handleCardClick;
