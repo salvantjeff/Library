b41b3817c9f28c1c6df84f0487fb7dccb0faebd4
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 16:41:27 -0500

    Create google sign up button

---
+++ src/landingPage/GoogleButton.js
@@ -0,0 +1,15 @@
+import googleIcon from "../../images/google_icon.png";
+
+const SignUpWithGoogleButton = () => {
+    const button = document.createElement('div');
+    button.classList.add('sing-up-with-google');
+    const text = document.createElement('p');
+    text.textContent = 'Sign up with Google';
+    const icon = document.createElement('img');
+    icon.setAttribute('src', googleIcon);
+    button.appendChild(icon);
+    button.appendChild(text);
+    return button;
+};
+
+export { SignUpWithGoogleButton };
