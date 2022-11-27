2fcdb36906215ece1f130518a7b2cdf6908df420
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 12:35:18 -0500

    Remove landingPage module

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,16 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import Dialog from "./popup/Dialog";
-import LandingPage from "../landingPage/LandingPage";
 import "../style.css";
 
 const Home = () => {
     const home = document.createElement('div');
     home.classList.add('home-page');
+    home.classList.add('hide');
     home.append(Header());
     home.append(Main());
     home.append(Footer());
     home.append(Dialog());
-    home.append(LandingPage());
     return home;
 };
 
