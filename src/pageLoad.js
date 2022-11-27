878136bf07710f17efe96db765d0be293de0a4a9
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 00:01:03 -0500

    Remove commented code

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,15 +1,8 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Dialog from "./components/popup/Dialog";
-import Footer from "./components/Footer";
-import "./style.css";
+import Home from "./components/Home";
 
 const initPageLoad = () => {
     const doc = document.body;
-    doc.append(Header());
-    doc.append(Dialog());
-    doc.append(Main());
-    doc.append(Footer());
+    doc.append(Home());
 };
 
 export default initPageLoad;
