b652593605441873bbc9fd42fee458c5a78c7a63
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 12:13:33 -0500

    Add a log out button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import createHeading from "./popup/createHeading";
 
+const logOutButton = () => {
+    const button = document.createElement('button');
+    button.classList.add('log-out_button');
+    button.textContent = 'Log out';
+    return button;
+};
+
 const Header = () => {
     const header = document.createElement('header');
     header.append(createHeading('your library fortress'));
+    header.append(logOutButton());
     return header;
 };
 
