6d7800d3174632f1b5224affbfb2d2cdb1d167f4
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 12:37:46 -0500

    Remove Home module

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,9 +1,7 @@
-import Home from "./components/Home";
 import LibraryApp from "./components/LibraryApp";
 
 const initPageLoad = () => {
     const doc = document.body;
-    // doc.append(Home());
     doc.append(LibraryApp());
 };
 
