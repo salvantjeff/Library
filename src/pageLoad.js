ad3f57a918fb7b2e8213a27206298e37ac2951e9
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-27 12:37:17 -0500

    Import LibraryApp modlue

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,8 +1,10 @@
 import Home from "./components/Home";
+import LibraryApp from "./components/LibraryApp";
 
 const initPageLoad = () => {
     const doc = document.body;
-    doc.append(Home());
+    // doc.append(Home());
+    doc.append(LibraryApp());
 };
 
 export default initPageLoad;
